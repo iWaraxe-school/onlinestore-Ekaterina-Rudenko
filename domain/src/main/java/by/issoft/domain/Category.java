89802803cb81300cab952240e9f9d1294f303677
Ekaterina-Rudenko <katestasevich30@gmail.com>
2022-02-14 10:08:21 +0300

    Add classes Product,Category, Store

---
+++ domain/src/main/java/by/issoft/domain/Category.java
@@ -0,0 +1,30 @@
+package by.issoft.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Category {
+    private String name;
+    private List<Product> productList;
+
+    public Category(String name) {
+        this.name = name;
+        this.productList = new ArrayList<>();
+    }
+
+    public Category() {
+    }
+
+    public void addProduct(Product product) {
+        if (product!=null) {
+            this.productList.add(product);
+        }
+    }
+
+    public void addAllProducts(List<Product> productList) {
+        if (productList!=null) {
+                this.productList.addAll(productList);
+            }
+    }
+
+}
---
+++ domain/src/main/java/by/issoft/domain/Product.java
@@ -0,0 +1,55 @@
+package by.issoft.domain;
+
+import com.google.common.base.Preconditions;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Product {
+    public final double MIN_RATE = 0;
+    public final double MAX_RATE = 5;
+    private final String name;
+    private double rate;
+    private BigDecimal price;
+
+    public Product(String name, double rate, BigDecimal price) {
+        this.name = name;
+        this.rate = rate;
+        this.price = price;
+    }
+
+    public void setRate(double rate) {
+        Preconditions.checkArgument(rate >= MIN_RATE && rate <= MAX_RATE, "Invalid rate value");
+        this.rate = rate;
+    }
+
+    public void setPrice(BigDecimal price) {
+        Preconditions.checkArgument(price.doubleValue() > 0, "Price must be positive value");
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Double.compare(product.rate, rate) == 0 && Objects.equals(name, product.name) && Objects.equals(price, product.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, rate, price);
+    }
+}
